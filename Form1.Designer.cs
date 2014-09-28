592b4d90bdb8b5461a060e0c0906e09ee7413d46
sauron007 <heemel125@hotmail.com>
2014-09-28 12:38:55 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ImageToASCII_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonCreateAscii = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.openImageButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxReWidth = new System.Windows.Forms.TextBox();
+            this.textBoxReHeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.buttonCreateAscii);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(831, 598);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonCreateAscii
+            // 
+            this.buttonCreateAscii.Enabled = false;
+            this.buttonCreateAscii.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCreateAscii.Location = new System.Drawing.Point(653, 519);
+            this.buttonCreateAscii.Name = "buttonCreateAscii";
+            this.buttonCreateAscii.Size = new System.Drawing.Size(91, 67);
+            this.buttonCreateAscii.TabIndex = 4;
+            this.buttonCreateAscii.Text = "Create ASCII";
+            this.buttonCreateAscii.UseVisualStyleBackColor = true;
+            this.buttonCreateAscii.Click += new System.EventHandler(this.buttonCreateAscii_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(807, 501);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.openImageButton);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(81, 519);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(308, 67);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Load Image";
+            // 
+            // openImageButton
+            // 
+            this.openImageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openImageButton.Location = new System.Drawing.Point(255, 30);
+            this.openImageButton.Name = "openImageButton";
+            this.openImageButton.Size = new System.Drawing.Size(43, 22);
+            this.openImageButton.TabIndex = 2;
+            this.openImageButton.Text = "..";
+            this.openImageButton.UseVisualStyleBackColor = true;
+            this.openImageButton.Click += new System.EventHandler(this.openImageButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(243, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Image Files (*.bmp, *.jpg, *.png)|*.bmp;*.jpg;*.png;";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBoxReHeight);
+            this.groupBox2.Controls.Add(this.textBoxReWidth);
+            this.groupBox2.Enabled = false;
+            this.groupBox2.Location = new System.Drawing.Point(407, 519);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(205, 67);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resize";
+            // 
+            // textBoxReWidth
+            // 
+            this.textBoxReWidth.Location = new System.Drawing.Point(6, 39);
+            this.textBoxReWidth.Name = "textBoxReWidth";
+            this.textBoxReWidth.Size = new System.Drawing.Size(93, 22);
+            this.textBoxReWidth.TabIndex = 4;
+            // 
+            // textBoxReHeight
+            // 
+            this.textBoxReHeight.Location = new System.Drawing.Point(105, 39);
+            this.textBoxReHeight.Name = "textBoxReHeight";
+            this.textBoxReHeight.Size = new System.Drawing.Size(93, 22);
+            this.textBoxReHeight.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Width:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Heigth:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 598);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Image To ASCII Converter";
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button openImageButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonCreateAscii;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxReHeight;
+        private System.Windows.Forms.TextBox textBoxReWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageToASCII_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ImageToASCII imageToAscii;
+        string fileName;
+        
+        private void openImageButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                fileName = openFileDialog1.FileName;
+
+                buttonCreateAscii.Enabled = true;
+                groupBox2.Enabled = true;
+            }
+        }
+
+        private void buttonCreateAscii_Click(object sender, EventArgs e)
+        {
+            imageToAscii = new ImageToASCII(fileName);
+
+            resizer();
+            imageToAscii.Execute();
+
+            richTextBox1.Text = "";
+
+            for (int i = 0; i < imageToAscii.RGBCharPrinted.Length; i++)
+            {
+                richTextBox1.Text += imageToAscii.RGBCharPrinted[i];
+                richTextBox1.Text += Environment.NewLine;
+            }
+                
+        }
+        private void resizer()
+        {
+            if (textBoxReWidth.Text.Length != 0 && textBoxReHeight.Text.Length != 0)
+            {
+                int w = int.Parse(textBoxReWidth.Text);
+                int h = int.Parse(textBoxReHeight.Text);
+
+                if (w > 1000 || h > 1000)
+                    MessageBox.Show("Width or height cannot be greater than 1000");
+                else if (w == 0 || h == 0)
+                    MessageBox.Show("Width or height cannot be 0");
+                else
+                    imageToAscii.Resize(w, h);
+            }
+        }
+
+    }
+}
